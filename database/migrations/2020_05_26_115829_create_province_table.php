cd7a66b5e97851201b57a30b60868cbc3373d729
kim <kim.kundad@gmail.com>
2020-05-26 11:59:36 +0700

    first code

---
+++ database/migrations/2020_05_26_115829_create_province_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvinceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('province', function (Blueprint $table) {
+            $table->bigIncrements('PROVINCE_ID');
+            $table->string('PROVINCE_CODE');
+            $table->string('PROVINCE_NAME');
+            $table->integer('GEO_ID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('province');
+    }
+}
