9d839589a58ea2ae65f7d0c65a6f66aba73899e8
kim <kim.kundad@gmail.com>
2020-05-26 12:02:31 +0700

    first code

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
               'email' => 'admin@email.com',
               'password' => bcrypt('12345678'),
               'code_user' => 'UD123456',
+              'avatar' => '1483537975.png'
             ]
         ]);
     }
